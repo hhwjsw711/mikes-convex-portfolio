0a6414cbaba7b24b9c18c438e20afa068cd95344
Mike Cann <mike.cann@gmail.com>
2026-01-09 07:38:24 +0800

    added a disable crons

--- convex/crons.ts
+++ convex/crons.ts
@@ -3,36 +3,44 @@ import { internal } from "./_generated/api";
 
 const crons = cronJobs();
 
-// Refresh latest 20 YouTube videos every 12 hours
-crons.interval(
-  "refreshYouTubeLatest",
-  { hours: 12 },
-  internal.youtube.refreshLatest,
-  {}
-);
+// Check if crons should be disabled (useful for dev environments)
+// Set DISABLE_CRONS=true in your Convex deployment to disable all cron jobs
+const disableCrons = process.env.DISABLE_CRONS === "true";
 
-// Full refresh of all YouTube videos every 48 hours
-crons.interval(
-  "refreshYouTubeAll",
-  { hours: 48 },
-  internal.youtube.refreshAll,
-  {}
-);
+if (!disableCrons) {
+  // Refresh latest 20 YouTube videos every 12 hours
+  crons.interval(
+    "refreshYouTubeLatest",
+    { hours: 12 },
+    internal.youtube.refreshLatest,
+    {}
+  );
 
-// Refresh Convex Stack articles every hour (offset by 30 minutes)
-crons.hourly(
-  "refreshStack",
-  { minuteUTC: 30 },
-  internal.stack.refresh,
-  {}
-);
+  // Full refresh of all YouTube videos every 48 hours
+  crons.interval(
+    "refreshYouTubeAll",
+    { hours: 48 },
+    internal.youtube.refreshAll,
+    {}
+  );
 
-// Refresh X (Twitter) posts every 24 hours
-crons.daily(
-  "refreshX",
-  { hourUTC: 6, minuteUTC: 0 }, // 6 AM UTC daily
-  internal.x.refresh,
-  {}
-);
+  // Refresh Convex Stack articles every hour (offset by 30 minutes)
+  crons.hourly(
+    "refreshStack",
+    { minuteUTC: 30 },
+    internal.stack.refresh,
+    {}
+  );
+
+  // Refresh X (Twitter) posts every 24 hours
+  crons.daily(
+    "refreshX",
+    { hourUTC: 6, minuteUTC: 0 }, // 6 AM UTC daily
+    internal.x.refresh,
+    {}
+  );
+} else {
+  console.log("Cron jobs disabled via DISABLE_CRONS environment variable");
+}
 
 export default crons;
